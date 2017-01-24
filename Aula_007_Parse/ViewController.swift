08b97a91f731ca6fc86e34a0c6295bdd552fc020
Swift <swift@WozMac8.local>
2017-01-24 20:23:49 -0200

    inicial

--- Aula_007_Parse/ViewController.swift
+++ Aula_007_Parse/ViewController.swift
@@ -1,23 +1,80 @@
-//
-//  ViewController.swift
-//  Aula_007_Parse
-//
-//  Created by Swift on 24/01/17.
-//  Copyright © 2017 Swift. All rights reserved.
-//
-
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, XMLParserDelegate {
+    
+    //MARK: Propriedades
+    var arrayPessoas : [Dictionary<String,String>]=[]
+    var dicAluno = [String : String]()
+    var tagAtual = ""
+    var conteudoTag = ""
+    
+    //MARK: Outlets
+    @IBOutlet weak var minhaTextView: UITextView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let caminhoArquivo = Bundle.main.path(forResource: "dados", ofType: "xml")
+        let urlArquivo = URL(fileURLWithPath: caminhoArquivo!)
+        let meuXMLParse = XMLParser(contentsOf: urlArquivo)
+        
+        meuXMLParse?.delegate = self
+        meuXMLParse?.parse()
+        
+        
+
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    //MARK: Métodos de XMLParseDelegate
+    func parserDidStartDocument(_ parser: XMLParser) {
+        print("Interpretacao do documento XML iniciada\n")
+    }
+    
+    func parserDidEndDocument(_ parser: XMLParser) {
+        print("Interpretacao do documento XML finalizada")
+    }
+    
+    func parser(_ parser: XMLParser, didStartElement elementName: String, namespaceURI: String?, qualifiedName qName: String?, attributes attributeDict: [String : String] = [:]) {
+        
+        tagAtual = elementName
+        
+        if elementName=="nome" || elementName=="idade"{
+            conteudoTag = ""
+        }
+    }
+    
+    func parser(_ parser: XMLParser, didEndElement elementName: String, namespaceURI: String?, qualifiedName qName: String?) {
+        
+        if elementName=="nome"{
+            dicAluno["nome"]=conteudoTag
+            conteudoTag=""
+            tagAtual=""
+        }
+        
+        if elementName=="idade"{
+            dicAluno["idade"]=conteudoTag
+            conteudoTag=""
+            tagAtual=""
+        }
+        
+        if elementName=="aluno"{
+            arrayPessoas += [dicAluno]
+        }
+    }
+    
+    func parser(_ parser: XMLParser, foundCharacters string: String) {
+        
+        if tagAtual=="nome" || tagAtual=="idade" {
+            conteudoTag = string
+        }
+    }
+    
+    //MARK: Actions
+    @IBAction func exibirDados(_ sender: UIButton) {
+        for lista in arrayPessoas{
+            minhaTextView.text! += "\(lista)\n"
+        }
     }
 
 
